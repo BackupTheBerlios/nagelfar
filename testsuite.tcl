64cdc976d7fb65414d8754d7b8267e30f00e2d95
peter <peter@20e60131-6e48-0410-8483-994970f483b4>
2004-03-30 14:10:58 +0000

    Moved all -setup into -body to get better error output.

--- testsuite.tcl
+++ testsuite.tcl
@@ -36,60 +36,55 @@ proc execTestFile {args} {
 
 test nagelfar-1.1 {
     Command line checks
-} -setup {
+} -body {
     createTestFile {
         set apa $bepa
     }
-} -body {
     execTestFile -fn _____
 } -returnCodes 1 -result {Could not find file '_____'}
 
 test nagelfar-1.2 {
     Command line checks
-} -setup {
+} -body {
     createTestFile {
         set apa $bepa
     }
-} -body {
     execTestFile -encoding gurkmeja
 } -returnCodes 1 -result {*Bad encoding name: "gurkmeja"*} -match glob
 
 test nagelfar-1.3 {
     Command line checks
-} -setup {
+} -body {
     createTestFile {
         set apa bepa
     }
-} -body {
     execTestFile -encoding ascii
 } -returnCodes 0 -result {%%}
 
 test nagelfar-2.1 {
     Basic functionality
-} -setup {
+} -body {
     createTestFile {
         set bepa 2
         set apa $bepa
         return -1
         return -code error -1
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-2.2 {
     Basic functionality
-} -setup {
+} -body {
     createTestFile {
         set apa $bepa
     }
-} -body {
     execTestFile -filter *Unknown*
 } -result {%%}
 
 test nagelfar-2.3 {
     Basic functionality
-} -setup {
+} -body {
     createTestFile {
         proc apa {w} {
             variable Priv
@@ -100,58 +95,53 @@ test nagelfar-2.3 {
             incr Priv(repeated)
         }
     }
-} -body {
     execTestFile
 } -result {%%}
 
 
 test nagelfar-3.1 {
     Basic errors
-} -setup {
+} -body {
     createTestFile {
         set apa $bepa
     }
-} -body {
     execTestFile
 } -result {%%Line   2: E Unknown variable "bepa"}
 
 test nagelfar-3.2 {
     Basic errors
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
         }
         set apa [hej a b]
     }
-} -body {
     execTestFile
 } -result {%%Line   4: E Wrong number of arguments (2) to "hej"}
 
 test nagelfar-3.3 {
     Basic errors
-} -setup {
+} -body {
     createTestFile {
         list apa [list a b [list a b]] a]
         list apa [list a b [list a b] a]]
     }
-} -body {
     execTestFile
 } -result {%%Line   2: N Unescaped end bracket*Line   3: N Unescaped end bracket} -match glob
 
 test nagelfar-3.4 {
     Basic errors
-} -setup {
+} -body {
     createTestFile {
         set apa bepa
         set cepa [set $apa]
     }
-} -body {
     execTestFile
 } -result {%%Line   3: N Suspicious variable name "$apa"}
 
 test nagelfar-3.5 {
     Basic errors
-} -setup {
+} -body {
     createTestFile {
         proc apa {w} {
             set Miffo(gurka) 1
@@ -160,7 +150,6 @@ test nagelfar-3.5 {
             incr Miffo(gurka)
         }
     }
-} -body {
     execTestFile
 } -result {%%Line   5: E Unknown variable "Miffo(hampa)"}
 
@@ -237,21 +226,20 @@ test nagelfar-4.7 {
 
 test nagelfar-5.1 {
     Procedure checking
-} -setup {
+} -body {
     createTestFile {
         proc info {apa} {
             return $apa
         }
         info hejsan
     }
-} -body {
     execTestFile
 } -result {%%Line   2: W Procedure "info" does not match previous definition*}\
         -match glob
 
 test nagelfar-5.2 {
     Procedure checking
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
             return $a
@@ -261,13 +249,12 @@ test nagelfar-5.2 {
         set z 3
         hej $x $y $z
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-5.3 {
     Procedure checking, detecting upvar
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
             upvar $b apa
@@ -278,13 +265,12 @@ test nagelfar-5.3 {
         set z 3
         hej $x $y $z
     }
-} -body {
     execTestFile
 } -result {%%Line   9: N Suspicious variable name "$y"}
 
 test nagelfar-5.4 {
     Procedure checking, detecting upvar
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
             upvar $a apa
@@ -294,13 +280,12 @@ test nagelfar-5.4 {
         set z 3
         hej x $y $z
     }
-} -body {
     execTestFile
 } -result {%%Line   8: E Unknown variable "x"}
 
 test nagelfar-5.5 {
     Procedure checking, detecting upvar
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
             upvar $c apa
@@ -311,13 +296,12 @@ test nagelfar-5.5 {
         hej $y $z x
         list $x
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-5.6 {
     Procedure checking, "wrong order"
-} -setup {
+} -body {
     createTestFile {
         proc hej {a b c} {
             return [hopp apa bepa cepa]
@@ -326,136 +310,124 @@ test nagelfar-5.6 {
             return $a
         }
     }
-} -body {
     execTestFile -flags -2pass
 } -result {%%Line   3: E Wrong number of arguments (3) to "hopp"}
 
 test nagelfar-6.1 {
     Expression checking
-} -setup {
+} -body {
     createTestFile {
         expr {1 + ""}
     }
-} -body {
     execTestFile
 } -result {%%Line   2: E Bad expression: can't use empty string as operand of "+"}
 
 test nagelfar-6.2 {
     Expression checking
-} -setup {
+} -body {
     createTestFile {
         set apa 1
         expr {1 + $apa /}
     }
-} -body {
     execTestFile
 } -result {%%Line   3: E Bad expression: premature end of expression}
 
 test nagelfar-6.3 {
     Expression checking
-} -setup {
+} -body {
     createTestFile {
         set apa 10
         set bepa 5
         # This gave a divide by zero error in the first implementation
         expr {1 / ($apa - $bepa)}
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-6.4 {
     Expression checking
-} -setup {
+} -body {
     createTestFile {
         set apa 10
         expr {$apa == {$bepa}}
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-7.1 {
     Command: upvar
-} -setup {
+} -body {
     createTestFile {
         upvar 1 bepa
     }
-} -body {
     execTestFile
 } -result {%%Line   2: E Wrong number of arguments (2) to "upvar"}
 
 test nagelfar-7.2 {
     Command: upvar
-} -setup {
+} -body {
     createTestFile {
         set x hej
         upvar 1 bepa $x
     }
-} -body {
     execTestFile
 } -result {%%Line   3: N Suspicious upvar variable "$x"}
 
 test nagelfar-7.3 {
     Command: upvar
-} -setup {
+} -body {
     createTestFile {
         set x hej
         upvar $x $x bepa
     }
-} -body {
     execTestFile
 } -result {%%Line   3: N Non constant level to upvar: "$x"}
 
 test nagelfar-8.1 {
     Variable handling
-} -constraints knownbug -setup {
+} -constraints knownbug -body {
     createTestFile {
         proc hej {x y} {
             global item
             list item($x,$y)
         }
     }
-} -body {
     execTestFile
 } -result {should detect missing dollar}
 
 test nagelfar-8.2 {
     Variable handling, -novar flag
-} -setup {
+} -body {
     createTestFile {
         proc hej {x y} {
             set apa bepa
             set cepa apa
         }
     }
-} -body {
     execTestFile -flags -novar
 } -result {%%}
 
 test nagelfar-9.1 {
     if statement, as comment
-} -setup {
+} -body {
     createTestFile {
         if 0 {
             set y $x
         }
         expr {$y}
     }
-} -body {
     execTestFile
 } -result {%%Line   5: E Unknown variable "y"}
 
 test nagelfar-9.2 {
     if statement, as comment
-} -setup {
+} -body {
     createTestFile {
         if 0 then {
             set y $x
         }
         expr {$y}
     }
-} -body {
     execTestFile
 } -result {%%Line   5: E Unknown variable "y"}
 
@@ -463,32 +435,30 @@ test nagelfar-10.1 {
     Brace alignment 
 } -constraints { 
     knownbug
-} -setup {
+} -body {
     createTestFile "
         cmd xx yy \\
             apa {
                hejsan
             }
     "
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-10.2 {
     Brace alignment 
-} -setup {
+} -body {
     createTestFile {
         list xx yy {
             hejsan
          }
     }
-} -body {
     execTestFile
 } -result {%%Line   4: N Close brace not aligned with line 2 (8 9)}
 
 test nagelfar-11.1 {
     Line numbers
-} -setup {
+} -body {
     createTestFile "
         list xx yy \\
                 zz \$y
@@ -501,22 +471,20 @@ test nagelfar-11.1 {
                     \$x
         }
     "
-} -body {
     execTestFile
 } -result {^%%Line\s+3:.*Line\s+4:.*Line\s+10:} -match regexp
 
 test nagelfar-11.2 {
     Line numbers, line 1
-} -setup {
+} -body {
     createTestFile {apa bepa
     }
-} -body {
     execTestFile
 } -result {%%Line   1: W Unknown command "apa"}
 
 test nagelfar-12.1 {
     Comments, bad in switch
-} -setup {
+} -body {
     createTestFile {
         switch apa {
             hej {
@@ -528,13 +496,12 @@ test nagelfar-12.1 {
             }
         }
     }
-} -body {
     execTestFile
 } -result "%%Line   6: W Switch pattern starting with #. This could be a bad comment.*" -match glob
 
 test nagelfar-12.2 {
     Comments, bad in list
-} -setup {
+} -body {
     createTestFile {
         array set apa {
             elem1 val1
@@ -542,13 +509,12 @@ test nagelfar-12.2 {
             elem2 val2
         }
     }
-} -body {
     execTestFile
 } -result "%%Line   4: N Suspicious \# char. Possibly a bad comment."
 
 test nagelfar-12.3 {
     Comments, bad in list
-} -setup {
+} -body {
     createTestFile {
         miffo apa {
             elem1 val1
@@ -557,13 +523,12 @@ test nagelfar-12.3 {
         }
         proc miffo {a b} {}
     }
-} -body {
     execTestFile -flags -2pass
 } -result "%%Line   4: N Suspicious \# char. Possibly a bad comment."
 
 test nagelfar-12.4 {
     Comments, should not complain too much
-} -setup {
+} -body {
     createTestFile {
         if {[catch {
             set apa bepa
@@ -578,20 +543,18 @@ test nagelfar-12.4 {
             set apa bepa
         }
     }
-} -body {
     execTestFile
 } -result "%%"
 
 test nagelfar-13.1 {
     Syntax database, multiple ?
-} -setup {
+} -body {
     createTestFile {
         array set hej {1 2}
         array names hej
         array names hej *a*
         array names hej -regexp *a*
     }
-} -body {
     execTestFile
 } -result {%%}
 
@@ -621,7 +584,7 @@ test nagelfar-13.3 {
 
 test nagelfar-14.1 {
     Namespaces, procs in namespace
-} -setup {
+} -body {
     createTestFile {
         namespace eval apa {}
         proc apa::bepa {hej hopp} {
@@ -631,13 +594,12 @@ test nagelfar-14.1 {
             set apa [bepa $hej $hopp]
         }
     }
-} -body {
     execTestFile
 } -result {%%}
 
 test nagelfar-14.2 {
     Namespaces, procs in namespace
-} -setup {
+} -body {
     createTestFile {
         namespace eval apa {
             proc bepa {hej hopp} {
@@ -648,7 +610,6 @@ test nagelfar-14.2 {
             set apa [bepa $hej $hopp]
         }
     }
-} -body {
     execTestFile
 } -result {%%}
 
@@ -656,7 +617,7 @@ test nagelfar-14.3 {
     Namespaces, imported procs
 } -constraints { 
     knownbug
-} -setup {
+} -body {
     createTestFile {
         namespace eval apa {}
         proc apa::bepa {hej hopp} {
@@ -667,7 +628,6 @@ test nagelfar-14.3 {
             set apa [bepa $hej $hopp]
         }
     }
-} -body {
     execTestFile
 } -result {%%}
 
