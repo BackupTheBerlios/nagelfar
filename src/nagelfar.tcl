ba33808d33bb37549eb790be18f2e682ae9fd9c6
peter <peter@20e60131-6e48-0410-8483-994970f483b4>
2005-01-30 23:22:57 +0000

    Bumped revision to 1.1.2.

--- src/nagelfar.tcl
+++ src/nagelfar.tcl
@@ -28,7 +28,7 @@ set debug 0
 package require Tcl 8.4
 
 package provide app-nagelfar 1.0
-set version "Version 1.1.1+ 2005-01-29"
+set version "Version 1.1.2 2005-01-31"
 
 set thisScript [file normalize [file join [pwd] [info script]]]
 set thisDir    [file dirname $thisScript]
