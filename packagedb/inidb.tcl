074bfef972304fc5e461e2600219a7ad1fdc0668
Peter Spjuth <peter.spjuth@gmail.com>
2012-09-30 22:10:19 +0200

    Added definition for inifile package

---
+++ packagedb/inidb.tcl
@@ -0,0 +1,18 @@
+# This syntax definition for the inifile package in TclLib
+# package require inifile 0.2.3
+# Contributed by Ruediger Haertel
+
+##nagelfar syntax ini::open x x
+##nagelfar syntax ini::close x
+##nagelfar syntax ini::commit x
+##nagelfar syntax ini::revert x
+##nagelfar syntax ini::filename x
+##nagelfar syntax ini::sections x
+##nagelfar syntax ini::keys x x
+##nagelfar syntax ini::get x x
+##nagelfar syntax ini::exists x x x?
+##nagelfar syntax ini::value x x x x?
+##nagelfar syntax ini::set x x x x
+##nagelfar syntax ini::delete x x x?
+##nagelfar syntax ini::comment x x x? x?
+##nagelfar syntax ini::commentchar x?
